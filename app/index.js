a0ebda4340fed3eb5dfffbc7d3cb191321a435fa
Brian Nkhata <bnkhata@mra.mw>
2023-04-12 11:19:01 +0200

    initial commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { userState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = userRouter();
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen />
+    </SafeAreaView>
   );
 };
 
 export default Home;
- 
